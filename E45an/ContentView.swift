e741b3e17e21283def1f514c1725d9c18e6c4984
christinejohanson <christine.johanson@yahoo.com>
2024-01-10 20:34:48 +0100

    Initial Commit

---
+++ E45an/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  E45an
+//
+//  Created by Christine Johanson on 2024-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E45an/E45anApp.swift
@@ -0,0 +1,17 @@
+//
+//  E45anApp.swift
+//  E45an
+//
+//  Created by Christine Johanson on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct E45anApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
